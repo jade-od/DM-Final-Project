53cedd4ee821a561f02c74b0d22eb9eaea8c23ec
Quinn Fricko <149716274+quinnfricko@users.noreply.github.com>
2025-07-11 12:22:28 +0900

    Add files via upload

---
+++ expense_tracker.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE AttendanceTracker;
+USe AttendanceTracker;
+
+Create Table students
+(Student_ID INT NOT NULL, 
+ Student_First_name varchar(20),
+ Student_last_name varchar(20),
+ CONSTRAINT students_pk PRIMARY KEY (student_ID));
+
+CREATE TABLE courses 
+   (CRN INT NOT NULL,
+    start_time DATETIME,
+    end_time DATETIME,
+    course_name varchar(20),
+    CONSTRAINT courses_pk PRIMARY KEY (CRN));
+
+CREATE TABLE attendance
+	( CRN INT NOT NULL, 
+	Student_id INT NOT NULL, 
+Date_of_attendance DATE NOT NULL,
+Attended varchar(3), 
+CONSTRAINT attendance_pk PRIMARY KEY (CRN, Student_ID, Date_of_attendance), 
+CONSTRAINT attendance_fk1 FOREIGN KEY (CRN) REFERENCES courses(CRN), 
+CONSTRAINT attendance_fk2 FOREIGN KEY (Student_ID) REFERENCES Students(Student_ID));
