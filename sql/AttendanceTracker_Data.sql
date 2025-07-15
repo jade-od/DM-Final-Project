73e92ac65f648b5573760984a915bce7fb2e9e8e
Quinn Fricko <149716274+quinnfricko@users.noreply.github.com>
2025-07-15 19:14:56 +0900

    Update AttendanceTracker_Data.sql

--- sql/AttendanceTracker_Data.sql
+++ sql/AttendanceTracker_Data.sql
@@ -1,13 +1,13 @@
 Use AttendanceTracker;
+
 INSERT INTO students VALUES
 (1, 'Quinn', 'Fricko'),
 (2, 'Olivia', 'Walton'),
-(3, ' Jade ', 'ODowd');
+(3, 'Jade', 'ODowd');
 
 INSERT INTO courses VALUES
 (1001, '10:00-11:00', 'Biology'),
 (1002, '11:00-12:00', 'Database Management');
-
 
 INSERT INTO attendance VALUES
 (1001, 1, '2025-07-13', 'Present'),
